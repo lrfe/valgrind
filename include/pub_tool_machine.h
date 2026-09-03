256f3ad3c401547412bda600a7c35eef721355d7
Ali Ahmet Memiş <aamemis@proton.me>
2026-09-03 04:10:44 +0300

    or1k: allow 8-byte instructions
    A branch and its delay slot are decoded as one 8-byte instruction, which
    cachegrind and callgrind rejected.

--- include/pub_tool_machine.h
+++ include/pub_tool_machine.h
@@ -103,7 +103,7 @@
 
 #elif defined(VGP_or1k_linux)
 #  define VG_MIN_INSTR_SZB          4
-#  define VG_MAX_INSTR_SZB          4
+#  define VG_MAX_INSTR_SZB          8 /* a branch plus its delay slot */
 #  define VG_CLREQ_SZB             20
 #  define VG_STACK_REDZONE_SZB      0
 
